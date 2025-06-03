c360785e6eebbc48c8cca7e1e963852ece3d9c44
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2025-06-03 10:42:55 +0100

    Update tests/conftest.py
    Co-authored-by: Sukhil Patel <42407101+Sukh-P@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -85,7 +85,7 @@ def sites(db_session):
     db_session.add(site)
     sites.append(site)
 
-    # Although this site is an indai site,
+    # Although this site is an india site,
     # we want it to be in the test data so we adjust the lat and lon
     site = SiteSQL(
         client_site_id=1,
