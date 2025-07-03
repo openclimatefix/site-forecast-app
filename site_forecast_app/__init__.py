3fc298fc7e2b8f96cab78d5bc17a8dcf3e3ea4e5
ram-from-tvl <ramfromtvl@gmail.com>
2025-07-03 13:21:32 +0530

    Fix: removed try-cache block

--- site_forecast_app/__init__.py
+++ site_forecast_app/__init__.py
@@ -1,9 +1,7 @@
 """Site Forecast App."""
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    # For Python < 3.8
-    from importlib_metadata import version, PackageNotFoundError
+
+from importlib.metadata import version, PackageNotFoundError
+
 
 try:
     __version__ = version("pvnet-app")
