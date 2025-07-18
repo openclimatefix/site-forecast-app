edef2cfa28dc83d3035c4ad66d2492e5ee02a3ea
Sukhil Patel <42407101+Sukh-P@users.noreply.github.com>
2025-07-18 11:32:32 +0100

    Add ad pv model (#36)

--- tests/models/test_pydantic_models.py
+++ tests/models/test_pydantic_models.py
@@ -5,4 +5,4 @@ from site_forecast_app.models.pydantic_models import get_all_models
 def test_get_all_models():
     """Test for getting all models"""
     models = get_all_models()
-    assert len(models.models) == 4
+    assert len(models.models) == 5
