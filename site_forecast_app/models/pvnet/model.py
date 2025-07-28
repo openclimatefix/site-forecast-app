50648308d5d7da3f604d85c945cf9c5264c9ef04
Sukhil Patel <42407101+Sukh-P@users.noreply.github.com>
2025-07-28 14:55:10 +0100

    Update valid times (#46)

--- site_forecast_app/models/pvnet/model.py
+++ site_forecast_app/models/pvnet/model.py
@@ -158,8 +158,10 @@ class PVNetModel:
 
         normed_preds = np.concatenate(normed_preds)
         n_times = normed_preds.shape[1]
+
+        # t0 time not included in forecasts
         valid_times = pd.to_datetime(
-            [sample_t0 + dt.timedelta(minutes=15 * i) for i in range(n_times)],
+            [sample_t0 + dt.timedelta(minutes=15 * (i+1)) for i in range(n_times)],
         )
 
         # index of the 50th percentile, assumed number of p values odd and in order
