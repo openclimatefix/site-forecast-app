44f680a5ffead97ae3a44972dd0e61b4d0e15dcc
ram-from-tvl <ramfromtvl@gmail.com>
2025-07-28 20:08:12 +0530

    solved callback error

--- site_forecast_app/app.py
+++ site_forecast_app/app.py
@@ -32,7 +32,6 @@ sentry_sdk.set_tag("app_name", "site_forecast_app")
 sentry_sdk.set_tag("version", __version__)
 
 typer_app = typer.Typer()
-app = typer_app  # alias so @app.callback() works without changing decorators
 
 
 def get_sites(db_session: Session, country: str = "nl") -> list[LocationSQL]:
@@ -185,7 +184,7 @@ def app(
     )
 
 
-@app.callback()
+@typer_app.callback(invoke_without_command=True)
 def typer_options(ctx: typer.Context) -> None:
     """Typer callback to set command defaults."""
     ctx.default_map = {
@@ -195,7 +194,7 @@ def typer_options(ctx: typer.Context) -> None:
                 "--date",
                 "-d",
                 formats=["%Y-%m-%d-%H-%M"],
-                help="Date-Time (UTC) at which we make the prediction. Format: YYYY-MM-DD-HH-mm.",
+                help="Date-time (UTC) at which we make the prediction. Format: YYYY-MM-DD-HH-mm.",
             ).default,
             "write_to_db": typer.Option(
                 False,
@@ -223,7 +222,7 @@ def app_run(
 
     if timestamp is None:
         # get the timestamp now rounded down the nearest 15 minutes
-        # TODO better to have explicitly UTC time here?
+        # TODO better to have explicity UTC time here?
         timestamp = pd.Timestamp.now(tz="UTC").replace(tzinfo=None).floor("15min")
         log.info(f'Timestamp omitted - will generate forecasts for "now" ({timestamp})')
     else:
@@ -315,4 +314,4 @@ def app_run(
 
 
 if __name__ == "__main__":
-    app()
+    typer_app()
