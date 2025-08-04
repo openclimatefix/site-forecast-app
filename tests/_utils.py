99240047ad8550f86ce2d2d48981662f5dceca8c
ram-from-tvl <ramfromtvl@gmail.com>
2025-08-04 20:01:56 +0530

    test issues

--- tests/_utils.py
+++ tests/_utils.py
@@ -1,15 +1,26 @@
 """Testing utils."""
 
+import typer
 from typer.testing import CliRunner
 
 
-def run_typer_script(app, args: list[str], catch_exceptions: bool = False):
+def run_typer_script(app_func, args: list[str], catch_exceptions: bool = False):
     """Util to test typer scripts while showing the stdout."""
 
     runner = CliRunner()
 
+    # Create a temporary Typer app for testing
+    if callable(app_func) and not hasattr(app_func, '_add_completion'):
+        # It's a function, wrap it in a Typer app
+        temp_app = typer.Typer()
+        temp_app.command()(app_func)
+        app_to_test = temp_app
+    else:
+        # It's already a Typer app
+        app_to_test = app_func
+
     # We catch the exception here no matter what, but we'll reraise later if need be.
-    result = runner.invoke(app, args, catch_exceptions=True)
+    result = runner.invoke(app_to_test, args, catch_exceptions=True)
 
     # Without this the output to stdout/stderr is grabbed by typer's test runner.
     # print(result.output)
