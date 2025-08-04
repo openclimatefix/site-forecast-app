3e03a12447805dc90044b4c2664e5fbbb939bf8a
ram-from-tvl <ramfromtvl@gmail.com>
2025-08-04 20:04:08 +0530

    test issues

--- tests/_utils.py
+++ tests/_utils.py
@@ -11,21 +11,13 @@ def run_typer_script(app_func, args: list[str], catch_exceptions: bool = False):
 
     # Create a temporary Typer app for testing
     if callable(app_func) and not hasattr(app_func, '_add_completion'):
-        # It's a function, wrap it in a Typer app
         temp_app = typer.Typer()
         temp_app.command()(app_func)
         app_to_test = temp_app
     else:
-        # It's already a Typer app
         app_to_test = app_func
 
-    # We catch the exception here no matter what, but we'll reraise later if need be.
     result = runner.invoke(app_to_test, args, catch_exceptions=True)
-
-    # Without this the output to stdout/stderr is grabbed by typer's test runner.
-    # print(result.output)
-
-    # In case of an exception, raise it so that the test fails with the exception.
     if result.exception and not catch_exceptions:
         raise result.exception
 
