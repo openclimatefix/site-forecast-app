85f00aa263434ddf7ffcf2e2a9ef14a9d59ce8dc
Felix <137530077+felix-e-h-p@users.noreply.github.com>
2026-05-29 13:53:30 +0100

    Integrate freezegun for time control in tests
    Added time freezing for tests using freezegun.

--- tests/integration/test_adjuster.py
+++ tests/integration/test_adjuster.py
@@ -1,6 +1,7 @@
 """Test for adjuster.py"""
 
 from datetime import datetime
+from freezegun import freeze_time
 
 import pandas as pd
 import pytest
@@ -13,6 +14,8 @@ from site_forecast_app.adjuster import (
 )
 
 
+now = pd.Timestamp.now().floor("15min") + pd.Timedelta(minutes=1)
+
 def test_get_me_values_no_values(db_session, sites):
     """Check no ME results are found with no forecast or generation values"""
 
@@ -21,6 +24,7 @@ def test_get_me_values_no_values(db_session, sites):
     assert len(me_df) == 0
 
 
+@freeze_time(now)
 def test_get_me_values(db_session, sites, generation_db_values, forecasts, init_timestamp):  # noqa: ARG001
     """Check ME results are found"""
 
@@ -35,6 +39,7 @@ def test_get_me_values(db_session, sites, generation_db_values, forecasts, init_
     assert me_df["me_kw"][90] != 0
 
 
+@freeze_time(now)
 def test_get_me_values_15(db_session, sites, generation_db_values, forecasts, init_timestamp):  # noqa: ARG001
     """Check ME results are found"""
 
@@ -83,6 +88,7 @@ def test_get_me_values_no_forecasts(db_session, sites, generation_db_values, ini
     assert len(me_df) == 0
 
 
+@freeze_time(now)
 def test_adjust_forecast_with_adjuster(db_session, sites, generation_db_values, forecasts):  # noqa: ARG001
     """Check forecast gets adjuster"""
     forecast_meta = {"timestamp_utc": datetime.now(), "location_uuid": sites[0].location_uuid}  # noqa: DTZ005
