c0fe8e516e40edf72f9942afc723a20c750dc950
Peter Dudfield <peter.dudfield@hotmail.com>
2026-07-31 11:18:40 +0530

    update test

--- tests/end_to_end/test_app.py
+++ tests/end_to_end/test_app.py
@@ -222,7 +222,7 @@ def test_app_ruvnl(
     result = run_click_script(app, args)
     assert result.exit_code == 0
 
-    n = 3  # 1 site, 3 wind models (2 GenCast + 1 ECMWF)
+    n = 1  # 1 site, 1 wind models (1 ECMWF) (there used to be 2 Gencast models)
     assert db_session.query(ForecastSQL).count() == init_n_forecasts + n * 2
     assert db_session.query(MLModelSQL).count() == n * 2
     forecast_values = db_session.query(ForecastValueSQL).all()
